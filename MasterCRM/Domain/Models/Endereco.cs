4afc4a41b33c0fcb8553d80298e990aff3215c09
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-10-11 20:51:25 -0300

    criado modelo de Endereço

---
+++ MasterCRM/Domain/Models/Endereco.cs
@@ -0,0 +1,12 @@
+﻿using MasterCRM.Domain.Base;
+
+namespace MasterCRM.Domain.Models;
+
+public class Endereco : EntidadeBase
+{
+    public string Rua { get; set; } = string.Empty;
+    public string Numero { get; set; } = string.Empty;
+    public string Cidade { get; set; } = string.Empty;
+    public string UF { get; set; } = string.Empty;
+    public string Cep { get; set; } = string.Empty;
+}
