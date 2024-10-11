4cacbed8971c2752cda11e1f90953714f7ae8abc
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-10-11 20:51:08 -0300

    criada entidade base

---
+++ MasterCRM/Domain/Base/EntidadeBase.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterCRM.Domain.Base;
+
+public class EntidadeBase
+{
+    public long Id { get; set; }
+    public bool EstaSalva { get; set; }
+    public string UsuarioCriacao { get; set; } = string.Empty;
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataAlteracao { get; set; }
+}
