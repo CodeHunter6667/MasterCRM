11d286610683092de4d4d78497b481d2277b5edb
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-10-11 20:52:42 -0300

    Criada classe de contexto

---
+++ MasterCRM/Data/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using MasterCRM.Domain.Contato;
+using MasterCRM.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace MasterCRM.Data;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<PessoaFisica> PessoasFisicas { get; set; }
+    public DbSet<PessoaJuridica> PessoaJuridicas { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
