76eeec308bf35a533a2a858102efac7ea4e936fa
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-10-21 20:52:11 -0300

    inicializado variavel

--- MasterCRM/Domain/Base/EntidadeBase.cs
+++ MasterCRM/Domain/Base/EntidadeBase.cs
@@ -3,7 +3,7 @@
 public class EntidadeBase
 {
     public long Id { get; set; }
-    public bool EstaSalva { get; set; }
+    public bool EstaSalva { get; set; } = false;
     public string UsuarioCriacao { get; set; } = string.Empty;
     public DateTime DataCriacao { get; set; }
     public DateTime? DataAlteracao { get; set; }
