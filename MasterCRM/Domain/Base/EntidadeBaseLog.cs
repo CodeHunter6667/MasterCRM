0a6894e27bb5100bd50c79704580468da17dd6ae
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-10-22 18:51:54 -0300

    criada classe base para classes que são registros de historico

---
+++ MasterCRM/Domain/Base/EntidadeBaseLog.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterCRM.Domain.Base;
+
+public class EntidadeBaseLog
+{
+    public long Id { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public string UsarioCriacao { get; set; } = string.Empty;
+}
